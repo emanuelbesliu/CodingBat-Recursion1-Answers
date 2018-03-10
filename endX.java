345ec89d8bf16295ce3b73289da9d5e314176886
emanuelbesliu <32497562+emanuelbesliu@users.noreply.github.com>
2018-03-10 13:42:32 +0000

    Create endX.java

---
+++ endX.java
@@ -0,0 +1,15 @@
+/**
+ * Given a string, compute recursively a new string where all the lowercase 'x' chars have been moved to the end of the string.
+ * 
+ *   endX("xxre") → "rexx"
+ *   endX("xxhixx") → "hixxxx"
+ *   endX("xhixhix") → "hihixxx"
+ **/
+ 
+ public String endX(String str) {
+  if(str.length()<=1) return str;
+  
+  if(str.charAt(0)=='x') return endX(str.substring(1))+str.charAt(0);
+  
+  return str.charAt(0)+endX(str.substring(1));
+}
